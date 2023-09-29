bc83ac3f13d18daa8e8d79c4f6a6a33af6fccd03
Henrik Sonnergård <henrik.sonnergard@sirvoy.com>
2023-09-29 10:04:40 +0200

    Update transformTokens.js
    Only keep css variables export

--- transformTokens.js
+++ transformTokens.js
@@ -46,28 +46,6 @@ const StyleDictionaryExtended = StyleDictionary.extend({
   ...deepMerge.all([androidConfig, webConfig]),
   source: ["tokens/*.json"],
   platforms: {
-    scss: {
-      transformGroup: "custom/css",
-      buildPath: "build/scss/",
-      files: [
-        {
-          destination: "_variables.scss",
-          format: "scss/variables",
-          filter: "validToken",
-        },
-      ],
-    },
-    less: {
-      transformGroup: "custom/css",
-      buildPath: "build/less/",
-      files: [
-        {
-          destination: "_variables.less",
-          format: "less/variables",
-          filter: "validToken",
-        },
-      ],
-    },
     css: {
       transformGroup: "custom/css",
       buildPath: "build/css/",
@@ -78,83 +56,6 @@ const StyleDictionaryExtended = StyleDictionary.extend({
           filter: "validToken",
           options: {
             showFileHeader: false,
-          },
-        },
-      ],
-    },
-    "json-flat": {
-      transformGroup: "js",
-      buildPath: "build/json/",
-      files: [
-        {
-          destination: "styles.json",
-          format: "json/flat",
-          filter: "validToken",
-        },
-      ],
-    },
-    ios: {
-      transformGroup: "ios",
-      buildPath: "build/ios/",
-      files: [
-        {
-          destination: "StyleDictionaryColor.h",
-          format: "ios/colors.h",
-          className: "StyleDictionaryColor",
-          type: "StyleDictionaryColorName",
-          filter: {
-            type: "color",
-          },
-        },
-        {
-          destination: "StyleDictionaryColor.m",
-          format: "ios/colors.m",
-          className: "StyleDictionaryColor",
-          type: "StyleDictionaryColorName",
-          filter: {
-            type: "color",
-          },
-        },
-        {
-          destination: "StyleDictionarySize.h",
-          format: "ios/static.h",
-          className: "StyleDictionarySize",
-          type: "float",
-          filter: {
-            type: "number",
-          },
-        },
-        {
-          destination: "StyleDictionarySize.m",
-          format: "ios/static.m",
-          className: "StyleDictionarySize",
-          type: "float",
-          filter: {
-            type: "number",
-          },
-        },
-      ],
-    },
-
-    "ios-swift-separate-enums": {
-      transformGroup: "ios-swift-separate",
-      buildPath: "build/ios-swift/",
-      files: [
-        {
-          destination: "StyleDictionaryColor.swift",
-          format: "ios-swift/enum.swift",
-          className: "StyleDictionaryColor",
-          filter: {
-            type: "color",
-          },
-        },
-        {
-          destination: "StyleDictionarySize.swift",
-          format: "ios-swift/enum.swift",
-          className: "StyleDictionarySize",
-          type: "float",
-          filter: {
-            type: "number",
           },
         },
       ],
